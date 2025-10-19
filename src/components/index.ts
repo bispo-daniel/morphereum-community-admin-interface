9ddab4566fd57bdf631943b7fc30e47fefedfbe1
DBS <dbispo.work@gmail.com>
2025-10-19 14:35:05 -0300

    add SkeletonButton and SkeletonP components; update index exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as ScrollToTop } from "./ScrollToTop";
 export { default as ProtectedRoute } from "./ProtectedRoute";
 export { default as DynamicIcon } from "./DynamicIcon";
+export { default as SkeletonButton } from "./SkeletonButton";
+export { default as SkeletonP } from "./SkeletonP";
